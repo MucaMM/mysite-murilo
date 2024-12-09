b42308afc50805827eaa87f3b4c300e90e411215
MucaMM <130078269+MucaMM@users.noreply.github.com>
2024-12-09 11:30:59 -0300

    update finish

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-// Aguarda o carregamento completo da página
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleciona o formulário pelo ID
     const form = document.getElementById("contactForm");
-    // Seleciona a mensagem de sucesso pelo ID
     const successMessage = document.getElementById("successMessage");
 
-    // Adiciona um evento de submissão ao formulário
     form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Impede o envio padrão do formulário
-        successMessage.classList.remove("hidden"); // Exibe a mensagem de sucesso
-        form.reset(); // Limpa os campos do formulário
+        event.preventDefault(); 
+        successMessage.classList.remove("hidden"); 
+        form.reset(); 
     });
 });
